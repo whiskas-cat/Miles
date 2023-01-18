d92e7e4d024b2bd06ef823d4eb842d22c9acc12f
Polina P <p.perlova87@gmail.com>
2023-01-18 22:00:06 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 12_565;
+
+        int miles = price / 20;
+
+        System.out.println("Бонусы:" + miles);
+    }
+}
